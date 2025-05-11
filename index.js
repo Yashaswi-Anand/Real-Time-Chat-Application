67d9dc46081553fc83df5db37d783730b712e1fe
Yashaswi Anand <yash.kwr17@gmail.com>
2025-05-11 23:40:23 +0530

    cors added

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const port = process.env.PORT || 8500;
 require('./config/mongoose')
 app.use(express.json());
 const cors = require('cors');
-const { Server } = require("socket.io");
-const User = require("./models/user");
 const users_router = require("./routers/user_routes");
 const message_router = require("./routers/message_router");
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use('/users/v1', users_router)
 app.use('/messages/v1', message_router)
@@ -18,6 +21,6 @@ app.get('/', (req,res) =>{
     return res.send('Server is healthy...');
 })
 app.listen(port, function(err){
-    if(err) {console.log("error");}
+    if(err) {console.log("error in starting server");}
     console.log(`Server is running on port: ${port}`);
 })
